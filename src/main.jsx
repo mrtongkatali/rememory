e262fa1175efdd80655792416a7ecd5e85adc5d5
Leo Diaz <leoangelo.diaz@gmail.com>
2024-07-04 19:58:37 +0800

    Initial logic

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './scss/index.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // Strict mode invoking twice on development
+  // https://stackoverflow.com/questions/60618844/react-hooks-useeffect-is-called-twice-even-if-an-empty-array-is-used-as-an-ar
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
